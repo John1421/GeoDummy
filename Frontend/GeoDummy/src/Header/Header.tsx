838ace1eac1a3ae9ba45d3d7676c5455673f71a9
iris1615 <irisescoval1615@gmail.com>
2025-12-03 11:27:57 +0000

    logo not appearing bug fixed

--- Frontend/GeoDummy/src/Header/Header.tsx
+++ Frontend/GeoDummy/src/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import EditMenu from "./EditMenu";
+import logo from "../assets/logo.png";
 const BUTTON_STYLE = " text-white text-shadow-lg  font-semibold py-2 px-4 rounded-lg hover:bg-[#39AC73] transition justify-between";
 function Header({ setBaseMapUrl }: { setBaseMapUrl: (url: string) => void }) {
   const [open, setOpen] = useState(false);
@@ -20,7 +21,7 @@ function Header({ setBaseMapUrl }: { setBaseMapUrl: (url: string) => void }) {
       </div>
 
       <img
-        src="src/assets/logo.png"
+        src={logo}
         alt="Logo"
         className="h-10 w-20 object-contain transform scale-250 mr-10"
       />
